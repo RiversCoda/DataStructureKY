e9dfcf07306a33b667a6be1dd39ae48cbc5ed8df
RiversCoda <630192690@qq.com>
2023-09-24 14:20:38 +0800

    3

---
+++ CH02/2-2-03.cpp
@@ -0,0 +1,62 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#define MAXSIZE
+using namespace std;
+
+typedef struct
+{
+    int num[30];
+    int length;
+} Sqlist;
+
+void delete_value(Sqlist &L, int x)
+{
+    int filled_pos = 0;
+    int delete_cnt = 0;
+    for (int i = 0; i < L.length; i++) {
+        int flag = 0;
+        if (L.num[i] == x) {
+            flag = 1;
+            delete_cnt++;
+        }
+        if (flag == 0) {
+            L.num[filled_pos] = L.num[i];
+            filled_pos++;
+        }
+    }
+    L.length -= delete_cnt;
+}
+
+int main()
+{
+    Sqlist L;
+    // Initialize
+    // Fill the array with random integers between 1 and 100
+    for (int i = 0; i < 30; i++) {
+        L.num[i] = rand() % 10 + 1;
+    }
+    L.length = 30;
+    // Output the original array
+    cout << "Original array:" << endl;
+    for (int i = 0; i < L.length; i++) {
+        cout << L.num[i] << " ";
+    }
+    cout << endl;
+    // porcess
+    // random x in array
+    int pos = rand() % L.length;
+    int x = L.num[pos];
+    cout << "delete x is " << x << endl;
+    delete_value(L, x);
+
+    // show
+    cout << "The array after process:" << endl;
+    for (int i = 0; i < L.length; i++) {
+        cout << L.num[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
